1c976e854f7d66d5a50204784cae14eb247e7742
Mftr96 <matteo.federici96@gmail.com>
2024-06-14 15:13:35 +0200

    Milestone 1 traccia 14/06/2024, ossia rinomazione file server.php in list.php; spostamento array da list.php a data.json;aggiornata chiamata axios in main.js

--- client/js/main.js
+++ client/js/main.js
@@ -5,15 +5,23 @@ createApp({
   data() {
     return {
       to_do:"",
+      task_completato:false,
+      // milestone 1 14/06/24:includo url api in data
+      apiURL:"../list.php",
     }
   },
   //funzioni richiamabili all' interno dell'app
   methods:{
+    get_done(index){
+      index=false;
+      console.log(index);
+    },
   },
   mounted(){
     //debug console log 
     console.log("app");
-    axios.get("../server.php")
+    //milestone 1 14/06/2024:aggiorno chiamata axios
+    axios.get(apiURL)
     .then(results=>{
       this.to_do=results.data;
     });
---
+++ list.php
@@ -0,0 +1,14 @@
+<?php
+
+//milestone 1 consegna 14/06/2024:sposto array in data.json
+//richiamo l'array dal file json in una variabile:
+$file_content=file_get_contents(("dati.json"));
+
+
+//stringa per importare poi contenuto
+header('Content-type: application/json');
+//converto array in formato JSON   
+$json_string=json_encode($to_do);
+//stampo risultato 
+echo $file_content;
+?>
